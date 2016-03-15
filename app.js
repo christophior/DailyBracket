3aa62c99023026dcd5161b7ae6a70618dd81c7ac
christophior <chrisvillarreal1018@gmail.com>
2016-03-15 16:54:54 -0500

    added some automation for grr.la

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var webdriver = require('selenium-webdriver'),
 
 
 var SIGNUP_URL = 'https://www.dailybracket.com/',
+    EMAIL_URL = 'https://www.guerrillamail.com',
     REFERAL_CODE = '3SFY';
 
 console.log("--- begin signup ---");
@@ -26,7 +27,9 @@ _.times(7, function(i) {
     driver.wait(until.elementIsVisible(driver.findElement(By.className('signupBtn'))), 5000);
     driver.findElement(By.className('signupBtn')).click();
 
-    var user_email = chance.email({domain: 'grr.la'});
+    var user_email = chance.email({length: 10, domain: 'grr.la'}),
+        emailWithoutDomain = user_email.split('@grr.la')[0];
+
     console.log('\tsigning up using: ' + user_email);
 
     driver.wait(until.elementLocated(By.name('email')), 5000);
@@ -38,6 +41,16 @@ _.times(7, function(i) {
     driver.findElement(By.name('state')).sendKeys('Texas');
     driver.findElement(By.name('referal')).sendKeys(REFERAL_CODE);
 
+    driver.wait(until.elementLocated(By.className('connect-btn')), 5000);
+    driver.wait(until.elementIsVisible(driver.findElement(By.className('connect-btn'))), 5000);
     driver.findElement(By.className('connect-btn')).click();
-    driver.quit();
+
+    // verify email
+    driver.get(EMAIL_URL);
+    driver.findElement(By.css('#gm-host-select > option:nth-child(3)')).click();
+    driver.findElement(By.css('span#inbox-id.editable.button')).click();
+    driver.findElement(By.xpath("/html/body/div[@id='guerrilla_mail']/div[@class='main-panel']/div[@class='show_address']/div[@class='col2']/span[@class='strong']/span[@id='inbox-id']/input")).sendKeys(emailWithoutDomain);
+    driver.findElement(By.css('button.save.button.small')).click();
+
+    // driver.quit();
 });
